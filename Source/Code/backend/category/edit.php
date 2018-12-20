c328d878c4730b0337dd54d4dd851ad95c99e3db
traphan98 <trapt62@wru.vn>
2018-12-20 22:56:37 +0700

    backend

---
+++ Source/Code/backend/category/edit.php
@@ -0,0 +1,67 @@
+
+<title>Category</title>
+<?php
+	include('../../db/connect.php');
+	include('../layout/header.php');
+	include('../layout/navbar.php');
+	include('../layout/menu.php');
+
+?>		
+
+		<noscript>
+			<div class="alert alert-block span10">
+				<h4 class="alert-heading">Warning!</h4>
+				<p>You need to have <a href="http://en.wikipedia.org/wiki/JavaScript" target="_blank">JavaScript</a> enabled to use this site.</p>
+			</div>
+		</noscript>			
+		<!-- start: Content -->
+		<div id="content" class="span10">
+		<ul class="breadcrumb">
+			<li>
+				<i class="icon-home"></i>
+				<a href="index.html">Home</a> 
+				<i class="icon-angle-right"></i>
+			</li>
+			<li><a href="#">Category</a></li>
+		</ul>
+		<div class="row-fluid sortable">
+				<div class="box span12">
+					<div class="box-header" data-original-title>
+						<h2><i class="halflings-icon white edit"></i><span class="break"></span>Add Category</h2>
+					</div>
+					<div class="box-content">
+						<form class="form-horizontal">
+						  <fieldset>
+							<div class="control-group">
+							  <label class="control-label" for="typeahead">Name Category</label>
+							  <div class="controls">
+								<input type="text" class="span6 typeahead" id="typeahead"  data-provide="typeahead" data-items="4" data-source='["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]'>
+							  </div>
+							</div>							
+							<div class="control-group">
+							  <label class="control-label" for="fileInput">File input</label>
+							  <div class="controls">
+								<input class="input-file uniform_on" id="fileInput" type="file">
+							  </div>
+							</div>          
+							<div class="control-group hidden-phone">
+							  <label class="control-label" for="textarea2">Textarea WYSIWYG</label>
+							  <div class="controls">
+								<textarea class="cleditor" id="textarea2" rows="3"></textarea>
+							  </div>
+							</div>
+							<div class="form-actions">
+							  <button type="submit" class="btn btn-primary">Save changes</button>
+							  <button type="reset" class="btn">Cancel</button>
+							</div>
+						  </fieldset>
+						</form>   
+
+					</div>
+				</div><!--/span-->
+
+			</div><!--/row-->
+	
+<?php
+	include('../layout/footer.php');
+?>
